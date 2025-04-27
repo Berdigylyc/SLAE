669483d13ccc0903b90a7ac0db78854533bc5733
Berdigylyc <redzhepdurdyev.phystech.edu>
2025-04-27 22:08:02 +0300

    Initial commit

--- CG CGS BiCG/1.py
+++ CG CGS BiCG/1.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Загрузка данных
 bicg_data = pd.read_csv("Bicg_times.csv")
 cg_data = pd.read_csv("CG_times.csv")
 
-# Построение графика
 plt.figure(figsize=(12, 6))
 
 plt.plot(bicg_data["Size"], bicg_data["time"], label="BiCG", marker='.', linestyle='-', color='blue')
 plt.plot(cg_data["Size"], cg_data["time"], label="CG", marker='.', linestyle='--', color='red')
 
-# Настройка оформления
+
 plt.title("Сравнение времени выполнения методов BiCG и CG")
 plt.xlabel("Размер матрицы ")
 plt.ylabel("Время, мс")
@@ -19,6 +17,5 @@ plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
 plt.tight_layout()
 
-# Сохранение или отображение
 plt.savefig("comparison_plot.png", dpi=300)
 plt.show()
